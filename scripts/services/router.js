d930a3709ac128a6f1964c0de46c732507265c1d
Craig Loftus <craig@regulusweb.com>
2018-06-20 16:24:43 +0100

    Refactor recruit.js into a multi service router

--- scripts/services/router.js
+++ scripts/services/router.js
@@ -1,15 +1,21 @@
-var recruit_service_keywords = ['hire', 'recruit', 'employ'];
-
 global.main = function() {
     // Process all incoming messages and determine the correct service
     // to route the user to.
+
+    // Each route is an array consisting of the name of a contact var at which
+    // to find the service to route to and an array of keywords to match against
+    var routes = [
+        ['recruit_service', ['hire', 'recruit', 'employ']],
+        ['offer_service', ['job']],
+        ['accept_service', ['offer']],
+    ];
 
     var msg_contains = function(word) {
         // Test whether the message (global) contains the given word
         var msg = (message.content || '').toLowerCase();
         return msg.indexOf(word.toLowerCase()) !== -1;
     };
-    
+
     var invoke_with_msg = function(service_id) {
         // Invoke a service by ID and pass the incoming message to it for context
         var service_obj = project.initServiceById(service_id);
@@ -19,9 +25,16 @@ global.main = function() {
             message_id: message.id,
         });
     }
-    
-    if (recruit_service_keywords.some(msg_contains)) {
-        invoke_with_msg(contact.vars.recruit_service);
-        return_value = true; // Don't fall through to next service
+
+    for (var i=0, l=routes.length; i<l; i++) {
+        var service_var = routes[i][0],
+            keywords = routes[i][1],
+            service_id = contact.vars[service_var];
+
+        if (service_id && keywords.some(msg_contains)) {
+            invoke_with_msg(service_id);
+            return_value = true; // Don't fall through to next service
+            break;
+        }
     }
 }
