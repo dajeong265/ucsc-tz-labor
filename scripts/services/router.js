8d0d41c2c612900cc1c1452b7b5a1b7a94e78509
Craig Loftus <craig@regulusweb.com>
2018-07-26 16:30:45 +0100

    Special case register service

--- scripts/services/router.js
+++ scripts/services/router.js
@@ -5,17 +5,17 @@ global.main = function() {
     // Each route is an array consisting of the name of a contact var at which
     // to find the service to route to and an array of keywords to match against
     var routes = [
-        ['recruit_service', ['hire', 'recruit', 'employ','kibarua','vibarua','wafanyakazi', 'wafanya', 'wafanya kazi', 'wafanya kaz']],
-        ['offer_service', ['job','kazi']],
-        ['view_service', ['offer','huduma']],
-        ['close_service', ['close','funga']],
-        ['survey_service', ['feedback','mrejesho']],
-        ['start_service', ['start', 'anza']],
-        ['stop_service', ['stop', 'sitisha']],
-        ['random_service', ['survey', 'utafiti']],
-        ['review_service', ['review', 'hakiki']],
-        ['register_service', ['register']],
-    ];
+            ['recruit_service', ['hire', 'recruit', 'employ','kibarua','vibarua','wafanyakazi', 'wafanya', 'wafanya kazi', 'wafanya kaz']],
+            ['offer_service', ['job','kazi']],
+            ['view_service', ['offer','huduma']],
+            ['close_service', ['close','funga']],
+            ['survey_service', ['feedback','mrejesho']],
+            ['start_service', ['start', 'anza']],
+            ['stop_service', ['stop', 'sitisha']],
+            ['random_service', ['survey', 'utafiti']],
+            ['review_service', ['review', 'hakiki']],
+        ],
+        register_keywords = ['register'];
 
     var msg_contains = function(word) {
         // Test whether the message (global) contains the given word
@@ -31,6 +31,12 @@ global.main = function() {
             event: 'incoming_message',
             message_id: message.id,
         });
+    }
+
+    if (register_keywords.some(msg_contains)) {
+        invoke_with_msg('SVca253f3b46ae138f'); // Register service id
+        global.return_value = true;
+        return;
     }
 
     for (var i=0, l=routes.length; i<l; i++) {
