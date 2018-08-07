d3a21c28b748e8f62dcd631e36f6117dbd294a7a
Dahyeon Jeong <dajeong@ucsc.edu>
2018-08-08 00:39:46 +0300

    Update action_1.js

--- scripts/services/review/action_1.js
+++ scripts/services/review/action_1.js
@@ -6,4 +6,23 @@ var code = utils.parse_code(content),
 
 if(offer) {
     global.$offer_id = offer.id;
+    
+    // get the recruitment details by the OFFER/HAKIKI code
+    var recruit_table = project.getOrCreateDataTable('recruit'),
+        recruit = recruit_table.getRowById(offer.vars.recruit_id);
+    
+    // Get the job posted date
+    var survey_table = project.getOrCreateDataTable('survey'),
+        surveys = survey_table.queryRows({
+            vars: {'recruit_id': offer.vars.recruit_id}
+        }),
+        survey = surveys.next();
+    
+    // Get the employer name
+    var employer = project.getContactById(recruit.contact_id);
+
+    // save variable details that will be used in the message contents.
+    global.$employer_name = employer.name;    
+    global.$payment = recruit.vars.payment;
+    global.$posted = moment.unix(survey.vars.posted).format('MMM Do');
 }
