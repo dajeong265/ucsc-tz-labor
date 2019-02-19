b61bd716e1264370b3344047759fcbc802d2f68e
Dahyeon Jeong <dajeong@ucsc.edu>
2019-02-19 18:03:33 +0300

    Update action_1.js

--- scripts/services/recruit/action_1.js
+++ scripts/services/recruit/action_1.js
@@ -6,10 +6,14 @@ var recruit = data.store_with_code(
         {
             'workers': state.vars.num_workers,
             'days': state.vars.num_days,
+            'crop': state.vars.crop,
             'type': state.vars.work_type,
             'payment': state.vars.payment,
             'acres': state.vars.acres,
             'start': state.vars.startday,
+            'employer_village': contact.vars.village,
+            'employer_number': from_number,
+            'employer_name': contact.name,
         }
     ),
     survey = data.store_with_code(
