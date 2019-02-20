7613158a7b257e8de71e012808f832178bb8337d
Dahyeon Jeong <dajeong@ucsc.edu>
2019-02-20 12:35:53 +0300

    Update action_1.js

--- scripts/services/offer/action_1.js
+++ scripts/services/offer/action_1.js
@@ -9,10 +9,13 @@ if (row) {
         var recruiter = project.getContactById(row.contact_id);
 
         global.$recruit_id = row.id;
+        global.$recruiter_wtp = row.vars.employer_wtp;
+        global.$payment = row.vars.payment;
+        
         global.$recruiter = recruiter.phone_number;
         global.$recruiter_name = recruiter.name;
-        global.$payment = row.vars.payment;
         global.$recruiter_village = recruiter.vars.village;
+
     }
     else {
         global.$closed = true;
